82ceda3aab7b59173b53bd3749f3850ec1411d2e
Rafael de Lima Tozo <rafaeldelimatozo@gmail.com>
2024-03-13 23:11:39 -0300

    criação da classe Musica

--- Album.java
+++ Album.java
@@ -3,9 +3,9 @@ import java.util.ArrayList;
 public class Album {
     private String nome;
     private int anoLancamento;
-    private ArrayList<String> musicas;
+    private ArrayList<Musica> musicas;
 
-    public Album (String nome, int anoLancamento, ArrayList<String> musicas){
+    public Album (String nome, int anoLancamento, ArrayList<Musica> musicas){
         this.nome = nome;
         this.anoLancamento = anoLancamento;
         this.musicas = musicas;
---
+++ Musica.java
@@ -0,0 +1,9 @@
+public class Musica {
+    private String nome;
+    private int duracao;
+
+    public Musica(String nome, int duracao){
+        this.nome = nome;
+        this.duracao = duracao;
+    }
+}
